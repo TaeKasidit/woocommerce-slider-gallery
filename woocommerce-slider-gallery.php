4ba4da42191a066097510e3a0d084247b4977c16
Brad West <brad@bradonomics.com>
2015-07-30 14:24:02 +0700

    init file.

--- woocommerce-slider-gallery.php
+++ woocommerce-slider-gallery.php
@@ -9,5 +9,17 @@ Author URI: https://github.com/TaeKasidit/
 License: GPLv3 - http://www.gnu.org/licenses/gpl.html
 */
 
-    if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+//* If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) exit;
 
+require_once( plugin_dir_path( __FILE__ ) . 'class-woocommerce-slider-gallery-display.php' );
+
+function woocommerce_slider_gallery_start() {
+
+  $post_notice = new WooCommerce_Slider_Gallery_Display();
+
+  $post_notice->initialize();
+
+}
+
+woocommerce_slider_gallery_start();
