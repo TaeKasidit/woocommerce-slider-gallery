52db83f86385be5caf009c9ed82b104a0d6aadfd
Brad West <brad@bradonomics.com>
2015-07-30 13:19:58 +0700

    Licence notice change.

--- woocommerce-slider-gallery.php
+++ woocommerce-slider-gallery.php
@@ -6,7 +6,7 @@ Description: Add a thumbnail slider (via FlexSlider) to product pages.
 Version: 0.0.0
 Author: TaeKasidit
 Author URI: https://github.com/TaeKasidit/
-License: GPLv3 - http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+License: GPLv3 - http://www.gnu.org/licenses/gpl.html
 */
 
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
